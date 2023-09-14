4e5d9017b3a0c118135a836ddba5afa0adc13af7
Amirhossein Mn <amirhossein@gamertag.app>
2023-09-14 12:55:50 +0330

    [ADD] Start a Working Express Server

--- myApp.js
+++ myApp.js
@@ -1,39 +1,12 @@
-let express = require('express');
+let express = require("express");
 let app = express();
+
+function say_hello(req, res) {
+  res.send("Response String");
+}
+
+app.get('/', say_hello);
 
 console.log("Hello World");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
