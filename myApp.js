c39fe85757934e0b7bbe2531bd6fb798c13823eb
Amirhossein Mn <amirhossein@gamertag.app>
2023-09-17 01:01:26 +0330

    [WIP] Serve JSON on a Specific Route

--- myApp.js
+++ myApp.js
@@ -8,8 +8,12 @@ function serve_file(req, res) {
   let INDEX_FILE_PATH = __dirname + "/views/index.html";
   res.sendFile(INDEX_FILE_PATH);
 }
-
 app.get("/", serve_file);
+
+function server_json(req, res) {
+  res.json({ message: "Hello json" });
+}
+app.get("/json", server_json);
 
 console.log("DONE!");
 
