d5fd5d4a2326ebc33d8cff6d4f585c4c3463e28a
Amirhossein Mn <amirhossein@gamertag.app>
2023-09-16 23:32:47 +0330

    [FIX] mount express.static middleware for /public

--- myApp.js
+++ myApp.js
@@ -1,14 +1,13 @@
 let express = require("express");
 let app = express();
 
+let ASSETS_FOLDER_PATH = __dirname + "/public";
+app.use("/public", express.static(ASSETS_FOLDER_PATH));
+
 function serve_file(req, res) {
   let INDEX_FILE_PATH = __dirname + "/views/index.html";
   res.sendFile(INDEX_FILE_PATH);
 }
-
-let ASSETS_FOLDER_PATH = __dirname + "/public";
-let STYLE_FILE_PATH = ASSETS_FOLDER_PATH + "/style.css";
-app.use(ASSETS_FOLDER_PATH, express.static(STYLE_FILE_PATH));
 
 app.get("/", serve_file);
 
