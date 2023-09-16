bf6ace9d465b92c369305a8e0fafbbf1297ecf89
Amirhossein Mn <amirhossein@gamertag.app>
2023-09-16 22:52:30 +0330

    [UPDATE] Serve Static Assets

--- myApp.js
+++ myApp.js
@@ -1,14 +1,17 @@
 let express = require("express");
 let app = express();
 
-let absolutePath = __dirname + "/views/index.html";
-
 function serve_file(req, res) {
-  res.sendFile(absolutePath);
+  let INDEX_FILE_PATH = __dirname + "/views/index.html";
+  res.sendFile(INDEX_FILE_PATH);
 }
+
+let ASSETS_FOLDER_PATH = __dirname + "/public";
+let STYLE_FILE_PATH = ASSETS_FOLDER_PATH + "/style.css";
+app.use(ASSETS_FOLDER_PATH, express.static(STYLE_FILE_PATH));
 
 app.get("/", serve_file);
 
-console.log("Hello World");
+console.log("DONE!");
 
 module.exports = app;
